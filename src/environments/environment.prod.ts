d567f50fea83347a455070c7f0a3d1bd8ee4523a
josecortes <josecortesdev@gmail.com>
2021-11-07 13:59:37 +0100

    Parar el tracking de environment

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  dominio: 'https://mykelt.com/',
-  backendLocal: 'http://127.0.0.1/',
-  herokuURL: 'https://ghastly-beast-50784.herokuapp.com/',
-
-  stripeId: 'pk_live_51JRiEIEDzo7bolN0lD3HL3bvc8EjTZFupOWKYFtStB7hLsBOy9DfkEjScIOMfxRMAnmZ4icgvJSSXsw9qlMLhrhD00NN8HoFSb',
-  cloudinaryUpload: 'https://api.cloudinary.com/v1_1/djqmzihzr/image/upload'
-};
